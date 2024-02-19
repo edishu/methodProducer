650dfe2bd94f7f0e49de94c408cfa3648794e982
Udayan Maurya <edmauryaishu@gmail.com>
2024-02-18 17:27:31 -0800

    parsed type

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,35 @@ const readXml = () => {
       return;
     }
     const parser = new XMLParser();
-    const parsedValue = parser.parse(data);
-    console.log(JSON.stringify(parsedValue));
+    const parsedValue: XmlJson = parser.parse(data);
+    console.log(JSON.stringify(parsedValue.root.row));
   });
 };
 readXml();
+
+type XmlJsonRow = {
+  Amount: string;
+  Employee: {
+    DOB: string;
+    DunkinBranch: string;
+    DunkinId: string;
+    FirstName: string;
+    LastName: string;
+    PhoneNumber: number;
+  };
+  Payee: {
+    LoanAccountNumber: number;
+    PlaidId: string;
+  };
+  Payor: {
+    ABARouting: number;
+    AccountNumber: number;
+    Address: { City: string; Line1: string; State: string; Zip: number };
+    DBA: string;
+    EIN: number;
+    Name: string;
+    DunkinId: string;
+  };
+};
+
+type XmlJson = { root: { row: XmlJsonRow[] } };
