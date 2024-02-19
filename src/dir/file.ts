c43a917783d0427f6bbef396eed396f51a0711ec
Udayan Maurya <edmauryaishu@gmail.com>
2024-02-18 17:19:03 -0800

    xml parser

--- src/dir/file.ts
+++
@@ -1,1 +0,0 @@
-export const someData = "somedata";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import { someData } from "@/dir/file";
+import { readFile } from "fs";
+import { XMLParser } from "fast-xml-parser";
 
-console.log(someData);
+const readXml = () => {
+  readFile(process.argv[2], "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const parser = new XMLParser();
+    const parsedValue = parser.parse(data);
+    console.log(JSON.stringify(parsedValue));
+  });
+};
+readXml();
